f4f304b27a625fb5526755638da74b5c3e6425a9
Julio Laiber de Freitas <julio-lafre@hotmail.com>
2024-05-29 14:07:32 -0300

    Configurando pasta base de templates e arquivos estáticos

--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates',
+            BASE_DIR / 'base_global'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'base_static',
+    
+)
 STATIC_URL = 'static/'
 
 # Default primary key field type
