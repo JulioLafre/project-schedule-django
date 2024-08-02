218f39fe9570da9cceb6753cf17cbf12d5febd6e
JulioLafre <julio-lafre@hotmail.com>
2024-08-02 13:37:50 -0300

    Criando ImageField e Configurando MEDIA_URL e MEDIA_ROOT

--- project/settings.py
+++ project/settings.py
@@ -121,10 +121,14 @@ USE_TZ = True
 
 
 STATICFILES_DIRS = (
-    BASE_DIR / 'base_static',
-    
+    BASE_DIR / 'base_static',   
 )
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
