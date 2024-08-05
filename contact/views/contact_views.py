372ad205708cb8c6548c03d204488dc72075ba98
JulioLafre <julio-lafre@hotmail.com>
2024-08-05 09:18:35 -0300

    Usando a request no template para pegar o valor de GET

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -24,11 +24,11 @@ def index(request):
 def search(request):
     search_value = request.GET.get('q', '').strip()
 
-    if len(search_value) < 1:
+    if search_value == '':
         return redirect('contact:index')
 
     contacts = Contact.objects \
-        .filter(show=True) \
+        .filter(show=True)\
         .filter(
             Q(first_name__icontains=search_value) |
             Q(last_name__icontains=search_value) |
@@ -38,14 +38,14 @@ def search(request):
         .order_by('-id')
 
     context = {
-        'contacts' : contacts,
-        'site_title' : 'Contatos -'
+        'contacts': contacts,
+        'site_title': 'Search -'
     }
 
     return render(
         request,
         'contact/index.html',
-        context 
+        context
     )
 
 
